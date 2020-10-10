024449e9571dcac28f471e62b6a8f9533e68d595
fafa-a <fablelong@yahoo.fr>
2020-10-10 18:49:47 +0200

    test function getAllInfo

--- controlers/sneaksControler.js
+++ controlers/sneaksControler.js
@@ -1,21 +1,9 @@
-const flightClub = require("../models/flightClub-scraper");
-const goat = require("../models/goat-scraper");
-const klekt = require("../models/klekt-scraper");
-const sneakersHeat = require("../models/sneakersHeat-scraper");
-const stockx = require("../models/stockx-scraper");
-const weTheNew = require("../models/weTheNew-scraper");
+const getAllInfo = require("../models/sneakerSearch");
 
 exports.list_results = async function (req, res) {
   const keyword = req.params.id;
-  const response = [
-    await stockx(keyword),
-    await klekt(keyword),
-    await goat(keyword),
-    await flightClub(keyword),
-    await sneakersHeat(keyword),
-    await weTheNew(keyword),
-  ];
+  const response = await getAllInfo(keyword);
   // const response = data;
   // res.send(response);
-  res.status(200).json({ results: response });
+  res.status(200).json(response);
 };
---
+++ utils/FS.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+
+const mkdir = function (dir) {
+  fs.mkdir(dir, { recursive: true }, (err) => {
+    if (err) throw err;
+  });
+};
+
+const writeFile = function (dir, files, data) {
+  fs.writeFile(`${dir + files}`, data, (error) => {
+    if (error) throw error;
+  });
+};
+
+exports.mkdir = mkdir;
+exports.writeFile = writeFile;
